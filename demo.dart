f3954ebb6f48e7255fbce93c5fc264b456a323de
Md. Abu Saad Linkon <linkon.iit@gmail.com>
2025-05-19 11:15:19 +0600

    create dart

---
+++ demo.dart
@@ -0,0 +1,27 @@
+void main(){
+  var name = "Linkon";
+  print(name);
+
+  //int age = 22;
+  //print(age);
+
+
+  var lastName;
+  print(lastName);
+
+
+dynamic marks='50';
+print(marks);
+
+var age= 22;
+var totalMarks=100;
+print(age+totalMarks);
+
+double pi=3.1416;
+String companyName='heeelo q';
+var msg = "jello";
+
+print(companyName);
+print(msg);
+
+ }
