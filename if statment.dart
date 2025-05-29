6e401a1feadf43c04c92bc1038b5bbf5f4f15b7f
Md. Abu Saad Linkon <linkon.iit@gmail.com>
2025-05-29 21:44:45 +0600

    if statement practice

---
+++ if statment.dart
@@ -0,0 +1,29 @@
+void main(){
+
+  //if statemnt
+  var num=-7;
+  if(num>0){
+    //print("Number is possitive");
+  }
+
+  //id else statemnt
+  if (num>0){
+    //print("number is possitive");
+  } else{
+   // print("Number is negative");
+  }
+
+  //else if
+
+  var signal="red";
+  if(signal=="red"){
+    print("Stop");
+  } else if(signal=="green"){
+    print("Go");
+  } else if(signal=="yellow"){
+    print("Please hold on");
+  } else{
+    print("Signal is not working");
+  }
+
+}
