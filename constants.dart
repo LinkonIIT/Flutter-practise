2406c122f018a2a8dd9942f9cb7d0c0f97217fb7
Md. Abu Saad Linkon <linkon.iit@gmail.com>
2025-05-29 21:29:43 +0600

    constant

---
+++ constants.dart
@@ -0,0 +1,18 @@
+void main(){
+  //final key without datatype
+  final firstName="Linkon";
+  //final keyword with datatype
+  final String lastName="Saad";
+
+  //print(firstName);
+  //print(lastName);
+
+  //conts==st keyword with datatype
+  const double per=90.5;
+  print(per);
+}
+
+class Student{
+  final marks=80;
+  static const semester=2;
+}
