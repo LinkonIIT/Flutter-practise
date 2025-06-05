2ef18ec02a67eccdd7644860ad5d5c4346d9054b
Md. Abu Saad Linkon <linkon.iit@gmail.com>
2025-06-05 21:23:01 +0600

    Switch Case

---
+++ coditonal stat.dart
@@ -0,0 +1,31 @@
+void main(){
+  
+var grade = "A";
+
+switch(grade){
+    case "A":{
+        print("Excilent");
+    }
+    break;
+    case "B":{
+        print("Good");
+    }
+    break;
+    case "C":{
+        print("Avg");
+    }
+    break;
+    case "D":{
+        print("Poor");
+    }
+    break;
+    case "F":{
+        print("You are fail");
+    }
+    break;
+    default:{
+        print("Invalid Grade, please Choose correct grade");
+    }
+}
+
+}
